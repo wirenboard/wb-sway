d4f66de643cabd9c568310bf49fb5f43efec0539
Anton Tarasov <mail@taraant.me>
2026-03-16 08:53:35 +0300

    sway: switch WB package to native source format

--- sway/main.c
+++ sway/main.c
@@ -185,6 +185,15 @@ static void log_kernel(void) {
 
 
 static bool drop_permissions(void) {
+	const char *allow_root = getenv("SWAY_ALLOW_ROOT");
+	bool sway_allow_root = allow_root && strcmp(allow_root, "1") == 0;
+
+	if (sway_allow_root) {
+		sway_log(SWAY_INFO,
+			"Running with root privileges because SWAY_ALLOW_ROOT=1");
+		return true;
+	}
+
 	if (getuid() != geteuid() || getgid() != getegid()) {
 		// Set the gid and uid in the correct order.
 		if (setgid(getgid()) != 0) {
