eb5a3492d4af2fe7359677e0d41154b0cf2ab8b8
Anton Tarasov <anton.tarasov@wirenboard.com>
2026-03-17 19:19:23 +0300

    wb-sway-kiosk: add HDMI audio setup

--- wb/usr/lib/wb-sway/wb-kiosk-prepare-config.sh
+++ wb/usr/lib/wb-sway/wb-kiosk-prepare-config.sh
@@ -16,6 +16,13 @@ if [ -z "$ff_mode" ] || [ "$ff_mode" = "null" ]; then
 	ff_mode="kiosk"
 fi
 
+volume=$(jq -r '.mod4.options.volume // 50' /etc/wb-hardware.conf 2>/dev/null || true)
+if [ -z "$volume" ] || [ "$volume" = "null" ]; then
+	volume=50
+fi
+
 printf '%s\n' "$url" > "${RUNTIME_CONFIG_DIR}/url"
 printf '%s\n' "$ff_mode" > "${RUNTIME_CONFIG_DIR}/ff_mode"
-chmod 0644 "${RUNTIME_CONFIG_DIR}/url" "${RUNTIME_CONFIG_DIR}/ff_mode"
+printf '%s\n' "$volume" > "${RUNTIME_CONFIG_DIR}/volume"
+chmod 0644 "${RUNTIME_CONFIG_DIR}/url" "${RUNTIME_CONFIG_DIR}/ff_mode" \
+	"${RUNTIME_CONFIG_DIR}/volume"
